7e471669c434b7cd5a8d045c9dafbff41bc5a420
RKOLegend <98464809+RKOLegend@users.noreply.github.com>
2022-02-17 19:15:48 +0530

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,13 @@
+
+print('Rate Minnal Challenge')
+print('1')
+print('2')
+print('3')
+a=int(input('Enter your choice:'))
+if a==1:
+    print('Amazing')
+elif a==2:
+    print('Awesome')
+else:
+    print('There are only good reviews')
+    
